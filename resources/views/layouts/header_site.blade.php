e8eba4bad7752739014f41f077eee7ebcb447919
Junior-Shyko <franciscoanto@gmail.com>
2021-01-30 15:40:50 -0300

    Adicionado link no header para uso no celualr

--- resources/views/layouts/header_site.blade.php
+++ resources/views/layouts/header_site.blade.php
@@ -12,10 +12,12 @@
                 <tr>
                   <td><i class="fa fa-phone"></i></td>
                   <td class="header-item-text">
+                    <a href="tel:{{$site[0]->sites_phoneFixed}}">
                     Ligue<br/>
                     
                     <span style="font-size: 14px;">
                     {{$site[0]->sites_phoneFixed}}
+                  </a>
                     </span>
                   </td>
                 </tr>
@@ -39,8 +41,10 @@
               <tr>
                 <td><i class="fa fa-whatsapp"></i></td>
                 <td class="header-item-text">
+                  <a href="https://api.whatsapp.com/send?phone=5585988101166&text=Ol%C3%A1,%20eu%20estou%20com%20uma%20d%C3%BAvida,%20poderia%20me%20ajudar?">
                   WhatsApp<br/>
                   <span style="font-size: 14px;">{{$site[0]->sites_phoneMobile}}</span>
+                </a>
                 </td>
               </tr>
             </table>
