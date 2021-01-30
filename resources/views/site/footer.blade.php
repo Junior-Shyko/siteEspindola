80b7b5db63af3f108603d24fbf3b82803b96bcf1
Junior-Shyko <franciscoanto@gmail.com>
2021-01-30 15:03:17 -0300

    Alterado o Footer

--- resources/views/site/footer.blade.php
+++ resources/views/site/footer.blade.php
@@ -67,13 +67,20 @@
                 <p class="footer-phone"><i class="fa fa-whatsapp"></i> 
                     +55 {{$site[0]->sites_phoneMobile}}</p>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-12 widget footer-widget newsletter">
-                <h4><span>Newsletter</span> <img src="{{ url('/img/site/divider-half.png') }}" alt="" /></h4>
-                <p><b>Se inscreva em nossa newsletter!</b> Enviamos mensalmente as principais notícias e novidades do mercado imobiliário.</p>
-                <form class="subscribe-form" method="post" action="#">
-                    <input type="text" name="email" value="Seu e-mail" />
-                    <input type="submit" name="submit" value="Enviar" class="button small alt" />
-                </form>
+            <div class="col-lg-3 col-md-3 col-sm-4 widget footer-widget text-center">
+                <a href="https://www.cactoscoworking.com.br/" target="_blank"><img src="https://static.wixstatic.com/media/224bc3_b3f675c182484cd49ea69a6480fc9dfb~mv2.png/v1/fill/w_98,h_79,al_c,q_85,usm_0.66_1.00_0.01/02.webp" alt="Cactos" /></a>
+                <p>  <br><strong>Cactos Coworking</strong><br>
+                    Rua Ari Barroso, 70 - Sala 212, Torre 1 <br>
+                    Papicu, Fortaleza - CE <br>
+                    contato@cactoscoworking.com.br <br>
+                    (85) 4042.0042 | 9.8800.4200 <br>
+                </p>
+                <div class="divider"></div>
+                <ul class="social-icons circle">
+                    <li><a href="https://www.google.com/maps?cid=18358593563149671868" target="_blank" title="Localização do cactos coworking"><i class="fa fa-map-marker"></i></a></li>
+                    <li><a href="https://www.instagram.com/cactoscoworking/" title="instagram do cactos coworking" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                    <li><a href="https://www.cactoscoworking.com.br/" title="Site do cactos coworking" target="_blank"><i class="fa fa-globe"></i></a></li>
+                </ul>
             </div>
         </div><!-- end row -->
     </div><!-- end footer container -->
