854bb42f63f1c38ee89a498f50e968b31f92b892
Junior-Shyko <franciscoanto@gmail.com>
2021-08-29 10:56:09 -0300

    Up na largura do box

--- resources/views/site/pages/indicate_friend.blade.php
+++ resources/views/site/pages/indicate_friend.blade.php
@@ -14,7 +14,7 @@
 <section class="module">
    <div class="container">
     <div class="row">
-        <div class="col-lg-8 col-md-8">
+        <div class="col-lg-12 col-md-12">
 
             <div class="blog-post">
               <a href="#" class="blog-post-img">
