1c74a1bbfae528c41998f068e9212919bc2273d8
Junior-Shyko <franciscoanto@gmail.com>
2022-03-12 12:35:29 -0300

    pacote de cookie

---
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -0,0 +1,28 @@
+<style>
+    .js-cookie-consent {
+        text-align: center;
+        padding: 5px;
+        font-size: 13px;
+    }
+    .js-cookie-consent-agree {
+        background-color: #48A0DC;
+        border-color: 1px solid #48A0DC !important;
+        border: 1px solid #48A0DC;
+        padding: 5px;
+        border-radius: 3px;
+        color: white;
+        font-weight: 600;
+    }
+</style>
+{{--  --}}
+<div class="js-cookie-consent cookie-consent">
+
+    <span class="cookie-consent__message">
+        Usamos cookies para personalizar conteúdos e melhorar a sua experiência. Ao navegar neste site, você concorda com a nossa Política de Cookies.
+    </span>
+
+    <button class="js-cookie-consent-agree cookie-consent__agree">
+        Aceitar
+    </button>
+
+</div>
