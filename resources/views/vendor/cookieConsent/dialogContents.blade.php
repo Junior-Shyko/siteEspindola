f2f7e7a08e1a40d790c38a48e71ae7408cf1a392
junior.oliveira <junior.oliveira@esp.ce.gov.br>
2022-08-15 10:26:59 -0300

    Aplicação de cookies no site

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -18,13 +18,108 @@
 
 <div class="js-cookie-consent cookie-consent">
 
+    <p> 
     <span class="cookie-consent__message">
-        Usamos cookies para personalizar conteúdos e melhorar a sua experiência. 
-        Ao navegar neste site, você concorda com a nossa Política de Cookies.
+        Utilizamos cookies para otimizar o funcionamento e a navegação de nosso site, 
+        analisar sua utilização e recomendar produtos e serviços que sejam do seu interesse.
     </span>
+    </p>
+    <p>
+        <span class="cookie-consent__message">
+            Ao clicar em "Aceitar todos os cookies", você concorda com nossa Política de Cookies.
+            Você poderá conhecer nossa Política de Privacidade e Política de Cookies.
+            <a href="http://" style="text-decoration: underline;" 
+            data-toggle="modal" data-target="#modalCookieDialod">clicando aqui.</a>
+        </span>
+    </p>
 
     <button class="js-cookie-consent-agree cookie-consent__agree">
-        Aceitar
+        Aceitar todos os cookies
     </button>
+<!-- Modal -->
+    <div class="modal fade" id="modalCookieDialod" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Espíndola Imobiliária</h4>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <label>1. O COMPROMISSO DO GRUPO ESPINDOLA COM A PROTEÇÃO DE DADOS.</label>
+                        <p>1.1 Nós somos a Espindola Imobiliária, especialista em locação e administração de imóveis.</p>
+                        <p>1.2 Redigimos esse Aviso de forma simples, para que você possa entender mais sobre
+                             os cookies coletados em nosso site. Caso você tenha qualquer dúvida sobre como
+                              tratamos seus dados pessoais, você pode acessar nosso Aviso de Privacidade ou, 
+                              ainda, entrar em contato conosco pelos nossos canais de atendimento.</p>
+                    </div>
+                    <div class="col-md-12">
+                        <label>2. SOBRE OS COOKIES COLETADOS PELO GRUPO.</label>
+                        <p>
+                            Sim, utilizamos cookies para fazer nosso site funcionar ou funcionar com mais 
+                            eficiência. Os cookies também podem nos fornecer informações a respeito do uso do 
+                            site. Cookies não causam dano nenhum ao seu computador. A maioria dos navegadores
+                             possibilita que o usuário, a qualquer instante, ative mecanismos para informá-lo 
+                             quando os cookies estiverem acionados ou para evitar que sejam acionados, embora 
+                             isso possa afetar a utilização de algumas funções de nosso site. As informações
+                              geradas pelos cookies geralmente contêm (i) o nome do site que o originou, (ii)
+                               o "tempo de vida" do cookie, ou seja, por quanto tempo ele permanecerá 
+                               armazenado em seu dispositivo, e (iii) um valor, que geralmente é um número
+                                único gerado aleatoriamente. Portanto, tais arquivos temporários armazenados 
+                                em seu computador contêm informações sobre o site acessado e não coletarão ou 
+                                armazenarão informações pessoais a seu respeito sem que você as tenha 
+                                fornecido; também não coletarão informações registradas em seu computador.
+                        </p>
+                        <p>
+                            Ao navegar no nosso site solicitaremos seu consentimento expresso para o uso de 
+                            cookies de desempenho, funcionalidade e publicidade, de acordo com a descrição 
+                            detalhada acima, exceto se você já tiver modificado a configuração do seu
+                             navegador de internet para rejeitar a utilização de cookies. Neste caso, 
+                             esclarecemos que a desativação de cookies poderá proporcionar um funcionamento 
+                             inadequado ou ineficiente das funcionalidades do nosso site.
+                        </p>
+                    </div>
 
+                    <div class="col-md-12">
+                        <label>3. DESEJA FALAR CONOSCO?</label>
+                        <p>
+                            3.1 Caso você tenha qualquer dúvida sobre este Aviso, entre em contato conosco 
+                            através do e-mail: <a href="mailto:">meajuda@espindolaimobiliaria.com.br</a>
+                        </p>
+                    </div>
+
+                    <div class="col-md-12">
+                        <label>4. MUDANÇA</label>
+                        <p>
+                            4.1. Este Aviso passa por atualizações minimamente anuais, para que possa refletir 
+                            os melhores padrões de privacidade e proteção de dados, bem como as efetivas 
+                            atividades de tratamento de dados pessoais realizadas pelo Grupo. Desta forma, 
+                            recomendamos a você que visite periodicamente esta página para se manter 
+                            atualizado em relação a eventuais modificações. Caso sejam feitas alterações 
+                            relevantes no presente documento, iremos publicar essa atualização, indicando 
+                            a data da última atualização.
+                        </p>
+                    </div>
+
+                    <div class="col-md-12">
+                        <label>5. LEI APLICÁVEL</label>
+                        <p>
+                            5.1. O presente Aviso será interpretado segundo a legislação brasileira, 
+                            inclusive, mas não se limitando à Constituição Federal, a LGPD, o Marco Civil 
+                            da Internet – Lei n. 12.965/2014 e o Código de Defesa do Consumidor – 
+                            Lei n. 8.078/199, no idioma português, sendo eleito o foro do seu domicílio para 
+                            dirimir qualquer litígio ou controvérsia envolvendo o presente documento, 
+                            salvo ressalva específica de competência pessoal, territorial ou funcional 
+                            pela legislação aplicável. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Sair</button>
+            </div>
+        </div>
+        </div>
+    </div>
 </div>
