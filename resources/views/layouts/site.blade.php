0c2a2187de45e484ee4b74f8bf9544c8dc59b22f
junior.oliveira <junior.oliveira@esp.ce.gov.br>
2022-08-15 10:27:34 -0300

    Conf para host local

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -8,7 +8,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="csrf-token" content="{{ csrf_token() }}" />
-      <link rel="icon" href="{{ url('public/img/site/favicon.png') }}">
+      <link rel="icon" href="{{ url('img/site/favicon.png') }}">
       <meta property="og:url" content="{{ $meta_site['url'] }}" />
       <meta property="og:title" content="{{ $meta_site['title'] }}" />
       <meta property="og:type" content="{{ $meta_site['type'] }}" />
